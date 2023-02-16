78b85e02a873021ea9ef09c327fb897b5222aa8a
John <105969398+zackniyo@users.noreply.github.com>
2023-02-16 22:18:54 +0200

    Added 2 files
    Added 2 files

---
+++ app.js
@@ -0,0 +1,57 @@
+// console.log('Javascript is fun')
+
+// let myName = 'Zach'
+
+// let sentence = `My name is ${myName} and i'm learning Js` 
+
+
+// alert(sentence)
+// console.log(sentence)
+
+// let userAge = 15;
+
+// if(userAge > 18 ){
+
+// alert('This person can drive')
+
+// }else {
+
+//     alert('This person cannot drive')
+// }
+
+// let userAnswer = prompt('Would you like to RSVP to our Party ?')
+
+
+// console.log(userAnswer);
+
+
+// if(userAnswer == 'yes') {
+//     console.log('We can not wait to see you there')
+
+
+// } else if (userAnswer == 'no') {
+
+//     console.log('Sorry, Maybe next time.')
+
+
+// }
+
+
+// let user1 = 'John Smith'
+// let user2 = 'PAUL Smith'
+// let user3 = 'ZACH Smith'
+
+
+// let users = ['John', 'Peter', 'Paul', 'Kathy', 12, true]
+
+// console.log(users)
+// console.log(users.length)
+// console.log(users[3])
+
+let countries = ['US', 'France']
+
+console.log(countries)
+
+countries.push('UK')
+
+console.log(countries)
